ae1f1bad231d26270b167650be8d44ee2a7009c2
passionlim <passion.lim@gmail.com>
2017-12-04 21:11:40 +0900

    Create Script_HW_5th_week_1st_day.m

---
+++ Script_HW_5th_week_1st_day.m
@@ -0,0 +1,29 @@
+## [Grammar HW] 2017.12.04
+
+I’d like to go on vacation with ~~3 members of~~ my family **which consists of 3 members**.  
+B/c 가족 전체 인원수를 나타내지 못함.  
+Ex) Our team plans to go on a training trip to Guam with all members of the team which consist of 20 members. 
+
+$350 per ~~a~~ person.  
+I ~~could~~ **would be able** to get the cheapest ticket for a round trip flight at $100 per ~~a~~ person, if ~~left~~ **leave** on **the** week days or if I ~~gained~~ **gain** early bird bargains.  
+Ex) When I went to the Seoul Land with my family, I purchased a one day pass online *at $15 per person*. And I would enjoy our time if the wether **were** warm. It was too cold to walk around the amusement park. So I promised to come again with my daughter next month. If the weather **is** good, we **will have** more great time.
+
+
+My company ~~supports~~ offers an attractive ~~welfare~~ **discount** for overseas hotels.   
+Ex) His grandma’s supporting her grandson to play violins well. And she has **offered various private lessons** for him since he was 7. 
+
+
+Osaka, **adjacent** to the sea, is **renowned** for its local delicacies.  
+Ex) Kang-Reung, adjacent to the sea and tall mountains, is renowned for various leisure activities.
+
+I would spend most ~~of~~ budget~~s~~ to enjoy those foods.  
+Ex) I invested in stocks with **some of my intuition** for next semester, but I lost some. I really felt sorry for my parents. I had no idea to how to solve this problem.
+
+After the trip, we must work out harder to stay in ~~our~~ shape~~s~~.   
+Ex) I reminisce my twenties when I **stayed in shape**.  
+Ex) There is no easy way to easily **stay in shape**. However, if you can keep up the pace, you’d get back your good figure of your youth.
+
+
+
+
+
