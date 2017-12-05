b81bb2193c20a42d9fa5d1dc598f3036189e9a7f
passionlim <passion.lim@gmail.com>
2017-12-05 23:33:21 +0900

    Create Script_HW_5th_week_2st_day.m

---
+++ Script_HW_5th_week_2st_day.m
@@ -0,0 +1,68 @@
+[Grammar HW] 2017.12.05
+
+## mistakes
+
+Similar **(to/with)**
+	Developing programs was regarded **similar with** constructing buildings. But Since the IT technology has been enhancing, the software development has been considered totally different from the building construction.
+
+Have an effect on + O / affect + O
+	How long you study doesn’t have effect on your English improvement. How to effectively study affects the improvement.
+	This template and strategy would affect your speech time to be reduced.
+	Even though his budget-conscious personality hasn’t never affected the impulsive purchase of her wife, however  
+
+As time ~~goes~~ **went** by
+	(과거에 일어나 시간이 지난 것을 묘사하는 경우 과거형으로 사용 )
+	I bought a computer when I entered a college. But **as time went by**, the computer had been worn down and then now it has been broken down. 
+
+In the past / in the future. 
+	Even though, the AI technology had been considered useless **in the past**, now it has become the new trend in the IT industry. We would face many home appliances using the technology **in the near future**. 
+
+that was the start of ~~my music tastes~~ **my music journey**.
+	The first time I played computer games, I was fascinated **by** computers. It was **the start of my computer geek journey**.
+
+Visited ~~to~~ theaters.
+	I planed to visit my parents’ house during last Thanksgiving holiday. However I wasn’t able to make it because my kid was sick.
+		
+	
+## difficult grammar ( A/The and singluar/plural )
+
+	Among various **IT technologies**, **the big-data technology** was famous in **the IT industry**, but nowadays **the AI technology** has developed a reputation for its convenience.
+
+	My friend and his wife had lead a rich life by his parent’s support. But he had not been interested in his familty. After all he become bankrupt because of **the impulsive consumption of his family**.
+
+	I have bucket list on witch I want to do with my kid before she become adolescent. One of them is going to **a top** of **a mountain** in South Korea together. So I plan to climb up onto **the top** of Hanla mountain in several years.
+
+	When I bought **a computer** that was famous at that time, I was excited because I was able to play **the computer games** that was listed on game charts at home. But I had to buy **a laptop** that was useful for my reports. I had used the laptop for a long time. In addition I also recall **the used laptop** that I bought from my friend. It is **the favorite one** among **computers** that I’ve used.
+
+
+## Have you experienced a problem while traveling? What was the problem, and how did you deal with it? 
+
+( Translate Korean to English )
+
+Topic : Travel, Park, Concert, Movie, Gathering, Home Improvement
+
+
+I tried to pull myself together, but I was embarrassed in my mind. To solve the problem, I found the location of the Korea embassy and I went there to explain my situation. They requested my ID on which my photo was attached and documents which identified myself such as my flight ticket. I gave them everything they wanted and fortunately they promised to issue the replacement  passport before I got on the plane.
+
+I learned from this experience that I should be careful. And now when I go on a trip, I avoid crowded places and double-check my belongings. 
+
+## describe the process of recycling 
+
+Intro ( Who / When)
+
+There are many house chores in my house. Among them, throwing out garbages and recycling is the most common house chore for men. I’m also responsible for recycling. Even though there is no regular day for recycling, I usually throw out them on weekends. 
+
+First of all, I collect recyclable materials in the laundary room. There are 3 recycling bins where I put each material respectively. I divide the bins for 3 purposes: paper, plastic, and the rest. Separated recycling bins help me to reduce time to finish my house chore.  
+
+On weekends, I bring those bins to the designated area beside my apartment. There are big bags for each material. I pour each recycling bin into the respective big bag. Sometimes I get my hands dirty with leaking drinks or remaining foods while picking up materials. Then, I return back home and wash my hands.
+
+Conclusion,  even though the recycling is tedious and disgusting for me, I have to do recycling because if I did’t, I would get fined by the government.
+
+
+
+
+
+
+
+
+## Script 
